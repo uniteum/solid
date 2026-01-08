2bf7f1422b6aa460b29b67d73619553fd46abc25
Paul Reinholdtsen <paul@bitsy.services>
2026-01-07 17:53:23 -0800

    AVOGADRO

--- src/Solid.sol
+++ src/Solid.sol
@@ -7,9 +7,9 @@ import {Clones} from "clones/Clones.sol";
 import {ReentrancyGuardTransient} from "reentrancy/ReentrancyGuardTransient.sol";
 
 contract Solid is ISolid, ERC20, ReentrancyGuardTransient {
-    uint256 constant MOL = 6.02214076e23;
+    uint256 constant AVOGADRO = 6.02214076e23;
     uint256 constant MOLS = 10000;
-    uint256 constant SUPPLY = MOLS * MOL;
+    uint256 constant SUPPLY = AVOGADRO * MOLS;
 
     ISolid public immutable NOTHING = this;
     uint256 public immutable MAKER_FEE = 0.001 ether;
@@ -23,7 +23,7 @@ contract Solid is ISolid, ERC20, ReentrancyGuardTransient {
 
     function withdraw(uint256 sol) external nonReentrant returns (uint256 eth) {
         (uint256 solPool, uint256 ethPool) = pool();
-        eth = ethPool - ethPool * solPool / (solPool + sol);
+        eth = ethPool - (ethPool * solPool) / (solPool + sol);
         _update(msg.sender, address(this), sol);
         emit Withdraw(this, sol, eth);
         (bool ok, bytes memory returnData) = msg.sender.call{value: eth}("");
@@ -41,27 +41,33 @@ contract Solid is ISolid, ERC20, ReentrancyGuardTransient {
     function deposit() public payable returns (uint256 sol) {
         (uint256 solPool, uint256 ethPool) = pool();
         uint256 eth = msg.value;
-        sol = solPool - solPool * (ethPool - eth) / ethPool;
+        sol = solPool - (solPool * (ethPool - eth)) / ethPool;
         _update(address(this), msg.sender, sol);
         emit Deposit(this, eth, sol);
     }
 
     receive() external payable {}
 
-    function made(string calldata name, string calldata symbol)
-        public
-        view
-        returns (bool yes, address home, bytes32 salt)
-    {
+    function made(
+        string calldata name,
+        string calldata symbol
+    ) public view returns (bool yes, address home, bytes32 salt) {
         if (bytes(name).length == 0 || bytes(symbol).length == 0) {
             revert Nothing();
         }
         salt = keccak256(abi.encode(name, symbol));
-        home = Clones.predictDeterministicAddress(address(NOTHING), salt, address(NOTHING));
+        home = Clones.predictDeterministicAddress(
+            address(NOTHING),
+            salt,
+            address(NOTHING)
+        );
         yes = home.code.length > 0;
     }
 
-    function make(string calldata name, string calldata symbol) external payable returns (ISolid sol) {
+    function make(
+        string calldata name,
+        string calldata symbol
+    ) external payable returns (ISolid sol) {
         if (this != NOTHING) {
             sol = NOTHING.make{value: msg.value}(name, symbol);
             require(sol.transfer(msg.sender, SUPPLY / 2), "Transfer failed");
@@ -70,13 +76,21 @@ contract Solid is ISolid, ERC20, ReentrancyGuardTransient {
             (bool yes, address home, bytes32 salt) = made(name, symbol);
             if (yes) revert MadeAlready(name, symbol);
             home = Clones.cloneDeterministic(address(NOTHING), salt, 0);
-            Solid(payable(home)).zzz_{value: msg.value}(name, symbol, msg.sender);
+            Solid(payable(home)).zzz_{value: msg.value}(
+                name,
+                symbol,
+                msg.sender
+            );
             sol = ISolid(payable(home));
             emit Make(sol, name, symbol);
         }
     }
 
-    function zzz_(string calldata name, string calldata symbol, address maker) external payable {
+    function zzz_(
+        string calldata name,
+        string calldata symbol,
+        address maker
+    ) external payable {
         if (bytes(_symbol).length == 0) {
             _name = name;
             _symbol = symbol;
